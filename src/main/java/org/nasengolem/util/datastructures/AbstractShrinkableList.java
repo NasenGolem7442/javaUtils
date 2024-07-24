d67b4b24fbda0f547eb9f0f014b765af868c5a7b
Paul Steinbach <PaulSteinbach@t-online.de>
2024-07-25 00:54:14 +0200

    added protected constructor to abstract class

--- src/main/java/org/nasengolem/util/datastructures/AbstractShrinkableList.java
+++ src/main/java/org/nasengolem/util/datastructures/AbstractShrinkableList.java
@@ -2,15 +2,71 @@ package org.nasengolem.util.datastructures;
 
 import java.util.AbstractList;
 import java.util.List;
-import java.util.RandomAccess;
+import java.util.NoSuchElementException;
+import java.util.Objects;
 
 /**
- * Interface for objects that can be resized to a smaller size. Note that this interface should
- * only be implemented
+ * This class extends the {@link AbstractList} skeleton lists that can implement {@link Shrinkable}.
+ * This means, that the end of the list can be cut off in constant time. The following idioms utilize the
+ * {@link #shrink(int)} method to increase performance when deleting the last elements of the list.
  *
+ * <pre>
+ *      list.removeLast(numberOfElements);
+ *      list.subList(from, size()).clear();
+ *      list.clear();
+ * </pre>
+ *
+ * @author Paul Steinbach
+ * @param <E> the type of elements in this list
  */
 public abstract class AbstractShrinkableList<E> extends AbstractList<E>
-        implements List<E>, RandomAccess {
-    abstract void shrink(int newSize);
+        implements List<E>, Shrinkable {
+    private static final String NOT_ENOUGH_ELEMENTS_MESSAGE = "Can't remove %d elements from a list of size %d.";
 
+    protected AbstractShrinkableList() {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @throws IllegalArgumentException {@inheritDoc}
+     */
+    @Override
+    public abstract int shrink(int newSize);
+
+    /**
+     * {@inheritDoc}
+     *
+     * @throws NoSuchElementException {@inheritDoc}
+     */
+    @Override
+    public E removeLast() {
+        E last = getLast();
+        removeLast(1);
+        return last;
+    }
+
+    @Override
+    protected void removeRange(int fromIndex, int toIndex) {
+        if (toIndex == size()) {
+            Objects.checkIndex(fromIndex, size());
+            shrink(fromIndex);
+        } else {
+            super.removeRange(fromIndex, toIndex);
+        }
+    }
+
+    /**
+     * Removes the last n elements from the list.
+     *
+     * @param numberOfElements the number of elements to remove
+     * @throws IllegalStateException if the list does not contain enough elements
+     */
+    public void removeLast(int numberOfElements) {
+        if (numberOfElements > size()) {
+            throw new IllegalStateException(NOT_ENOUGH_ELEMENTS_MESSAGE.formatted(numberOfElements, size()));
+            //TODO: Double-check if IllegalStateException is correct here
+        }
+        shrink(size() - numberOfElements);
+    }
 }
